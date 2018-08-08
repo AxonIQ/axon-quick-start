fea63e7f5378b02e1bf04af48b33f0bf57c0ade1
jgacas <jugoslav.gacas@gmail.com>
2018-08-08 22:45:27 +0200

    Added @Aggregate annotation to ChatRoom command model

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -5,10 +5,12 @@ import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
 import org.axonframework.commandhandling.model.AggregateLifecycle;
 import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.spring.stereotype.Aggregate;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Aggregate
 public class ChatRoom {
     @AggregateIdentifier
     private String roomId;
